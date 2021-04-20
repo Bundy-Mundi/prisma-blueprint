ff0cb14cabb0270cb918bcc4720979e26f21bf89
Bundy-Mundi <bennie.ben095@gmail.com>
2021-04-21 03:52:51 +0900

    Finish setting

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Instagram Clone 3.0
+
+## Stacks
+
+- prisma
+
+- apollo-server
+
+- graphql-tools
+
+## Environment Variable
+
+- `DATABASE_URL`: This is required to connect `postgresql` to `prisma`
+
+## Progress
+
+- [ ] Create Account
+- [ ] See Profile
+- [ ] Login
--- insta-backend/index.ts
+++ insta-backend/index.ts
@@ -1,11 +1,10 @@
-
+require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
 import schema from "./schema";
 // The GraphQL schema
-
-
+const PORT = process.env.PORT || 4000;
 const server = new ApolloServer({schema});
 
-server.listen().then((pt:{ url:string }) => {
+server.listen(PORT).then((pt:{ url:string }) => {
   console.log(`🚀 Server ready at ${pt.url}`);
 });
