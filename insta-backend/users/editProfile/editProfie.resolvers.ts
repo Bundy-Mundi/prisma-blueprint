89f5e05dfd0e1756e68eeb1756a43d69b954043e
Bundy-Mundi <bennie.ben095@gmail.com>
2021-04-28 21:05:46 +0900

    Handle undefined | null for mutation editProfile

--- insta-backend/users/editProfile/editProfie.resolvers.ts
+++ insta-backend/users/editProfile/editProfie.resolvers.ts
@@ -1,9 +1,9 @@
 require('dotenv').config();
-import { IResolvers } from "apollo-server"
+import { IResolvers } from "apollo-server";
 import client from "../../client";
 import bycrypt from "bcrypt";
 
-type AllowUndefined = string | undefined;
+type AllowUndefined = string | undefined | null;
 
 type EditProfileProp = {
     firstName: AllowUndefined
@@ -21,26 +21,42 @@ const EditProfileMutation: IResolvers = {
             username,
             email,
             password 
-        }: EditProfileProp) => {
+        }: EditProfileProp):Promise<BasicReturnType> => {
             try {
-
+                if(!username && !email){
+                    throw new Error("Wrong approach. Username and email cannot be 'null'.");
+                }
+                else if(!password){
+                    throw new Error("Wrong approach. You can't have an empty password.");
+                }
                 // Check if there is the same username or email
-                if(username !== undefined && email !== undefined){
-                const doesUserExist = await client.user.findUnique({where:{username}});
-                const doesEmailExist = await client.user.findUnique({where:{email}});
+                // firstName & lastName can be undefined
+                // password has to be more than 8 characters
+                if(username && email && password){
+                    const salt = process.env.SALT || 10;
+                    username = username.trim().replace(/\s/g, ''); // Remove whitespace
+                    password = password.trim().replace(/\s/g, ''); // Remove whitespace
+                    password = await bycrypt.hash(password, salt); // Hash Password
+
+                    // If firstName & lastName  are null | undefined alsways give them a empty string
+                    if(firstName && lastName){
+                        firstName = firstName.trim();
+                        lastName = lastName.trim();
+                    } else {
+                        firstName = "";
+                        lastName = "";
+                    }
+
+                    // Check recurrings
+                    const doesUserExist = await client.user.findUnique({where:{username}});
+                    const doesEmailExist = await client.user.findUnique({where:{email}});
                     if(doesUserExist){
                         throw new Error("The same username exists. Try others.")
                     } else if(doesEmailExist){
                         throw new Error("The same email exists. Try others.")
                     }
+                    client.user.update({where:{username, email}, data:{username, email, password, firstName, lastName}});
                 }
-
-                // Hash Password
-                if(password !== undefined){
-                    const salt = process.env.SALT || 10;
-                    password = await bycrypt.hash(password, salt);
-                }
-                client.user.update({where:{username, email}, data:{username, email}});
                 return {
                     ok: true
                 }
