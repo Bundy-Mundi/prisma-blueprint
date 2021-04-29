08a60f57934547cc13ecd00399f4e295d435eb6f
Bundy-Mundi <bennie.ben095@gmail.com>
2021-04-30 03:33:54 +0900

    Add currentUser & isLoggedIn to context

--- insta-backend/index.ts
+++ insta-backend/index.ts
@@ -1,9 +1,18 @@
 require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
 import schema from "./schema";
+import { getUser, isLoggedIn } from './users/users.utils';
 // The GraphQL schema
 const PORT = process.env.PORT || 4000;
-const server = new ApolloServer({schema});
+const server = new ApolloServer({ 
+  schema, 
+  context: async({req}) => {
+    return {
+      currentUser: await getUser(<string>req.headers.token),
+      isLoggedIn 
+    }
+  }
+});
 
 server.listen(PORT).then((pt:{ url:string }) => {
   console.log(`🚀 Server ready at ${pt.url}`);
